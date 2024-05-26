3cc6d0adfe460444c4bf4320311d706825803556
Wilver Arana R <wilver.ar.dev@gmail.com>
2024-05-26 17:26:11 -0500

    Initial commit

---
+++ src/main/java/com/upc/securebagapi/SecureBagApiApplication.java
@@ -0,0 +1,13 @@
+package com.upc.securebagapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBagApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureBagApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/securebagapi/SecureBagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.securebagapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBagApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
